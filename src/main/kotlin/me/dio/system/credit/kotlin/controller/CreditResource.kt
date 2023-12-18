c99f973c95fa9b5f46e505f05c0d85ed09164f70
Hugo Cezar <hugocssouza@gmail.com>
2023-12-17 21:05:44 -0400

    Criada a função saveCredit

--- src/main/kotlin/me/dio/system/credit/kotlin/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/system/credit/kotlin/controller/CreditResource.kt
@@ -1,7 +1,10 @@
 package me.dio.system.credit.kotlin.controller
 
+import me.dio.system.credit.kotlin.dto.CreditDto
+import me.dio.system.credit.kotlin.entity.Credit
 import me.dio.system.credit.kotlin.service.impl.CreditService
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,7 +14,9 @@ class CreditResource(
     private val creditService: CreditService
 ) {
     @PostMapping
-    fun saveCredit(creditDto: CreditDto): String{
+    fun saveCredit(@RequestBody creditDto: CreditDto): String{
+        val credit: Credit = this.creditService.save(creditDto.toEntity())
+        return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
 
     }
 }
