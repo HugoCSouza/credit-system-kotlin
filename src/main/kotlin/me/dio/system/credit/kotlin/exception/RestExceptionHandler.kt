1215fe00d271d21ec6ca97d3c7b26da380c910cf
Hugo Cezar <hugocssouza@gmail.com>
2023-12-17 23:39:05 -0400

    Criado o tratamento da excessão DataAccessException

--- src/main/kotlin/me/dio/system/credit/kotlin/exception/RestExceptionHandler.kt
+++ src/main/kotlin/me/dio/system/credit/kotlin/exception/RestExceptionHandler.kt
@@ -1,5 +1,6 @@
 package me.dio.system.credit.kotlin.exception
 
+import org.springframework.dao.DataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
@@ -30,4 +31,26 @@ class RestExceptionHandler {
         )
     }
 
+    @ExceptionHandler(DataAccessException::class)
+    fun handlerValidException(ex: DataAccessException): ResponseEntity<ExceptionDetails> {
+        return ResponseEntity(
+            ExceptionDetails(
+                title = "Conflict! Consult the documentation.",
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.toString(),
+                details = mutableMapOf(ex.cause.toString() to ex.message)
+            ), HttpStatus.BAD_REQUEST
+        )
+        /* O ResponseEntity pode ser criado tambem assim
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+        ExceptionDetails(
+            title = "Bad Request! Consult the documentation.",
+            timestamp = LocalDateTime.now(),
+            status = HttpStatus.BAD_REQUEST.value(),
+            exception = ex.javaClass.toString(),
+            details = mutableMapOf(ex.cause.toString() to ex.message)
+        ))
+         */
+    }
 }
